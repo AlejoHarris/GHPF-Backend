234e743cd11cedf6519e89499a777ce4a0a1a819
Alejandro Harris <alejandro-luis@harris-bonet@hp.com>
2024-07-03 01:07:39 +0200

    Update ENV Vars

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,13 +1,13 @@
 module.exports = {
-  HOST: "db",
-  USER: "root",
-  PASSWORD: "123456",
-  DB: "testdb",
-  dialect: "mysql",
+  HOST:     process.env.HOST,
+  USER:     process.env.USER,
+  PASSWORD: process.env.PASSWORD,
+  DB:       process.env.DB,
+  dialect:  "mysql",
   pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
+    max:      5,
+    min:      0,
+    acquire:  30000,
+    idle:     10000
   }
 };
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:8080"
+  origin: process.env.CORS 
 };
 
 app.use(cors(corsOptions));
